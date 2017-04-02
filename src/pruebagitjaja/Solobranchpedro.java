a9fb6b71ecb95586b95a26c1539f1016b9240e1b
PedroCN97 <pedro1997cn@gmail.com>
2017-04-02 11:02:35 -0600

    Prueba branch pedro

---
+++ src/pruebagitjaja/Solobranchpedro.java
@@ -0,0 +1,10 @@
+package pruebagitjaja;
+
+/**
+ * Created by pedro on 2/4/2017.
+ */
+public class Solobranchpedro {
+    public static void main(String[] ags){
+        System.out.print("hola mundo");
+    }
+}
